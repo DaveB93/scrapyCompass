088c5fa65acfafd64268befc37089a94db6a5c02
dave_b93 <dave_b93@hotmail.com>
2018-01-27 22:53:35 -0800

    first revision

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # scrapyCompass
 a scrapy crawler for checking a compass card balance and alerting if it is wrong
+
+# Setup
+
+requires python 3 and Scrapy
+Get scrapy here: https://scrapy.org/
+edit compass.py and input 
+ * your compass card
+ * your compass card cvn
+ * your email ( currently it's set up for gmail )
+ * your gmail password ( you'll want to generate an app specific password )
+ * your expected balance
+
+# Running
+
+To run:
+ scrapy runspider compass.py
+
+To automate:
+ 
+ on windows:
+  make a batch file with the above command and create a scheduled task
---
+++ compass.py
@@ -0,0 +1,63 @@
+from scrapy.item import Item, Field
+from scrapy.http import FormRequest
+from scrapy.spiders import Spider
+from scrapy.utils.response import open_in_browser
+import smtplib
+import email
+from email.message import EmailMessage
+
+emailAcct = ''
+emailPassword = ''
+expectedBalance = '$14.35' # expected value $XX.XX   ( I didn't strip off the $ at the front)
+compassCardNumber = ''
+compassCardCVN = ''
+
+
+def sendemail(expectedVal, val):
+    sender = emailAcct
+    receivers = [sender]
+    user = sender
+    passw = emailPassword
+
+    smtp_host = 'smtp.gmail.com'
+    smtp_port = 587
+    server = smtplib.SMTP()
+    server.connect(smtp_host,smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.login(user,passw)
+
+    msg = EmailMessage()
+    msg['From'] = sender
+    msg['To'] = ", ".join(receivers)
+    msg['Subject'] = "Compass Balance is different"  
+
+    msg.set_content('Compass balance should be \'' + expectedVal+ '\' instead it is \'' +  val + '\'')
+    server.sendmail(user,receivers,msg.as_string())
+
+
+class compassSpider(Spider):
+    name = "compasscard"
+    allowed_domains = ["compasscard.ca"]
+    start_urls = ["https://www.compasscard.ca"]
+
+    def parse(self, response):
+        formdata = {'ctl00$Content$ucCardInput$txtSerialNumber': compassCardNumber,
+                'ctl00$Content$ucCardInput$txtCvn': compassCardCVN, 
+                'ctl00$Content$btnCheckBalance':'Continue as guest'}
+        yield FormRequest.from_response(response,
+                                        formdata=formdata,
+                                        clickdata={'name': 'ctl00$Content$btnCheckBalance'},
+                                        callback=self.parse1)
+
+    def parse1(self, response):
+        #from scrapy.shell import inspect_response
+        #inspect_response(response, self)
+        val = response.css("span.value-text-style::text").extract()[0].strip()
+        if (val != expectedBalance):
+            sendemail(expectedBalance, val)
+        else:
+            print( "value is fine")
+        #open_in_browser(response)
+
+
