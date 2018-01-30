31988d43a23930a64d57a5406f903fc36dd4b322
Beleznay <David.Beleznay@relic.com>
2018-01-29 16:55:30 -0800

    - Configuration is now stored seperately - option to re-configure ( with defaults from previous config ) - track last balance and alert if it drops by more than X  ( for non monthly compass card holders )

--- README.md
+++ README.md
@@ -7,19 +7,38 @@ Requires python 3 and Scrapy
 
 Get scrapy here: [https://scrapy.org/]
 
-Edit compass.py and input 
+Before you run this, you should collect the following
 - your compass card
 - your compass card cvn
 - your email ( currently it's set up for gmail )
-- your gmail password ( you'll want to generate an app specific password )
-- your expected balance
+- your gmail password ( you'll want to generate an app specific password, see instructions below if you need help)
+- the maximum amount you expect your balance to change between runs
 
 ## Running
 
 ### To Run:
  `scrapy runspider compass.py`
 
+  if it is your first run, you will be asked to set up some info, which will be stored in a local file called compassConfig.ini
+
 ### To Automate:
  
 #### On Windows:
-  make a batch file with the above command and create a scheduled task
+  Make a batch file with the above command and create a scheduled task. 
+
+  I personally have it set to run once a day about an hour after I last usually travel.
+
+
+### To Re-set up:
+ Either modify the .ini file directly or run 
+  `scrapy runspider compass.py -a reconfig=1`
+
+
+## Generating an App password for your Google account
+
+1. Go to https://myaccount.google.com/security?utm_source=OGB&utm_medium=act#signin 
+1. Click on App passwords
+1. Verify your identity with Google
+1. in the dropdown at the bottom, enter a custom name (that you'll remember for the app password)
+1. click Generate
+1. make a copy of this password, you'll only get it once
--- compass.py
+++ compass.py
@@ -1,23 +1,20 @@
 from scrapy.item import Item, Field
-from scrapy.http import FormRequest
+from scrapy.http import FormRequest,Request
 from scrapy.spiders import Spider
 from scrapy.utils.response import open_in_browser
 import smtplib
 import email
 from email.message import EmailMessage
+import configparser
+import os
 
-emailAcct = ''
-emailPassword = ''
-expectedBalance = '$14.35' # expected value $XX.XX   ( I didn't strip off the $ at the front)
-compassCardNumber = ''
-compassCardCVN = ''
+configfileName = 'compassConfig.ini'
+INIT_STORED_VALUE = '$-9999'
+INIT_STORED_VALUE_AS_CENTS = -999900
 
-
-def sendemail(expectedVal, val):
-    sender = emailAcct
+def sendemail(sender, passw, subject, message):
     receivers = [sender]
     user = sender
-    passw = emailPassword
 
     smtp_host = 'smtp.gmail.com'
     smtp_port = 587
@@ -30,20 +27,50 @@ def sendemail(expectedVal, val):
     msg = EmailMessage()
     msg['From'] = sender
     msg['To'] = ", ".join(receivers)
-    msg['Subject'] = "Compass Balance is different"  
+    msg['Subject'] = subject
 
-    msg.set_content('Compass balance should be \'' + expectedVal+ '\' instead it is \'' +  val + '\'')
+    msg.set_content(message)
     server.sendmail(user,receivers,msg.as_string())
+
+def inputWithDefault(askFor, config, path):
+    splitpath = path.split('/')
+    if splitpath[0] in config and splitpath[1] in config[splitpath[0]]:
+        question = "{} [{}]:".format(askFor, config[splitpath[0]][splitpath[1]])
+        return input(question) or config[splitpath[0]][splitpath[1]]
+    else:
+        question = '{}:'.format(askFor)
+        return input(question)
 
 
 class compassSpider(Spider):
     name = "compasscard"
     allowed_domains = ["compasscard.ca"]
-    start_urls = ["https://www.compasscard.ca"]
+    config = configparser.ConfigParser()
+
+    def start_requests(self):
+        if os.path.isfile(configfileName):
+            self.config.read(configfileName)
+
+        if (hasattr(self, 'reconfig')) or (not os.path.isfile(configfileName)):
+            self.config["email"] = {
+                "account": inputWithDefault('gmail account', self.config, 'email/account'), 
+                "password": inputWithDefault('gmail account password', self.config, 'email/password')
+            }
+            self.config["compass"] = {
+                "compassCardNumber": inputWithDefault('Compass Card Number', self.config, 'compass/compassCardNumber'), 
+                "compassCardCVN": inputWithDefault('Compass Card CVN', self.config, 'compass/compassCardCVN'),
+                "compassCardMaxChange": inputWithDefault('Compass Card Stored Value allowed maximum change between runs (0 for Monthly pass)', self.config, 'compass/compassCardMaxChange'),
+                "compassCardLastStoredValue": INIT_STORED_VALUE
+            }
+            with open(configfileName, 'w') as cfgfile:
+                self.config.write(cfgfile)
+                cfgfile.close()
+        yield Request("https://www.compasscard.ca")
+
 
     def parse(self, response):
-        formdata = {'ctl00$Content$ucCardInput$txtSerialNumber': compassCardNumber,
-                'ctl00$Content$ucCardInput$txtCvn': compassCardCVN, 
+        formdata = {'ctl00$Content$ucCardInput$txtSerialNumber': self.config['compass']['compassCardNumber'],
+                'ctl00$Content$ucCardInput$txtCvn': self.config['compass']['compassCardCVN'], 
                 'ctl00$Content$btnCheckBalance':'Continue as guest'}
         yield FormRequest.from_response(response,
                                         formdata=formdata,
@@ -52,9 +79,24 @@ class compassSpider(Spider):
 
     def parse1(self, response):
         val = response.css("span.value-text-style::text").extract()[0].strip()
-        if (val != expectedBalance):
-            sendemail(expectedBalance, val)
+        cents_int = int(round(float(val.strip('$'))*100))
+        maxChangeCents = int(round(float(self.config['compass']['compassCardMaxChange'].strip('$'))*100))
+        compassCardLastStoredValue = int(round(float(self.config['compass']['compassCardLastStoredValue'].strip('$'))*100))
+        expectedBalance = compassCardLastStoredValue - maxChangeCents
+        if (compassCardLastStoredValue == INIT_STORED_VALUE_AS_CENTS):
+            message =  'Compass balance initialized it is \'{}\''.format(val)
+            sendemail(self.config['email']['account'], self.config['email']['password'], "Compass Balance initialized", message)
+        elif (cents_int < expectedBalance):
+            message =  'Compass balance should be \'${:.2f}\' instead it is \'${:.2f}\''.format(expectedBalance/100, cents_int/100)
+            sendemail(self.config['email']['account'], self.config['email']['password'], "Compass Balance is different", message)
         else:
             print( "value is fine")
+        self.config['compass']['compassCardLastStoredValue'] = val
+        with open(configfileName, 'w') as cfgfile:
+            self.config.write(cfgfile)
+            cfgfile.close()
+
+
+
 
 
